a748e932db34fad1d71f5484b8f2dff13c2c2497
jugen667 <julien.genty80@gmail.com>
2024-06-27 20:40:29 +0200

    temp files for test and debug

---
+++ temp/test4.c
@@ -0,0 +1,12 @@
+// Un exemple de programme MiniC
+int start = 0;
+int end = 5;
+
+void main () {
+	int i, s = start , e = end ;
+	int sum = 0;
+	do{
+		sum = sum + 1;
+	} while(sum<10);
+	print("sum :", sum , "\n") ;
+}
